f9a76da2adfd28321a4c80d5d52d41eb94901f87
chanyungpark <222356440+chanyungpark@users.noreply.github.com>
2025-12-09 11:42:59 -0800

    Complete Exercise 09
    This initial function for sumAll passes test 1

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = b; i <= a; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
